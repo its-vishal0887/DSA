9d4e543b2e3f05272d4871e4ca64591b49b7aaa6
Vishal Sarvade <196059382+its-vishal0887@users.noreply.github.com>
2026-08-01 10:42:19 +0530

    Time: 14 ms (28.03%), Space: 14.9 MB (40.89%) - LeetHub

--- 1691-maximum-height-by-stacking-cuboids/1691-maximum-height-by-stacking-cuboids.cpp
+++ 1691-maximum-height-by-stacking-cuboids/1691-maximum-height-by-stacking-cuboids.cpp
@@ -13,11 +13,23 @@ public:
         int exclu = 0 + solve(arr, prev, curr+1);
         return max(inclu,exclu);
     }
+    int solveUsingMemo(vector<vector<int>>&arr, vector<vector<int>>&dp, int prev, int curr){
+        if(curr >= arr.size()) return 0;
+        if(dp[curr][prev+1] != -1) return dp[curr][prev+1];
+        int inclu = 0;
+        if(prev == -1 || choose(arr[prev], arr[curr])){
+            inclu = arr[curr][2] + solveUsingMemo(arr, dp, curr, curr+1);
+        }
+        int exclu = 0 + solveUsingMemo(arr,dp, prev, curr+1);
+        dp[curr][prev+1] = max(inclu, exclu);
+        return dp[curr][prev+1];
+    }
     int maxHeight(vector<vector<int>>& arr) {
+        vector<vector<int>>dp(arr.size(), vector<int>(arr.size()+1, -1));
         for(auto &i : arr){
             sort(i.begin(), i.end());
         }
         sort(arr.begin(), arr.end());
-        return solve(arr, -1, 0);
+        return solveUsingMemo(arr,dp, -1, 0);
     }
 };
