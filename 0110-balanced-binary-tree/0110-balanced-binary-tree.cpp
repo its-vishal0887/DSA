3471d9e390fdcad594d313e0c3f2ccb40263732f
Vishal Sarvade <196059382+its-vishal0887@users.noreply.github.com>
2026-08-17 06:56:41 +0530

    Time: 0 ms (100%), Space: 23.2 MB (20.89%) - LeetHub

---
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.cpp
@@ -0,0 +1,27 @@
+
+class Solution {
+public:
+    int height(TreeNode* root){
+        if(root == NULL){
+            return 0;
+        }
+
+        int leftH = height(root->left);
+        int rightH = height(root->right);
+        return max(leftH, rightH) + 1;
+    }
+    bool isBalanced(TreeNode* root) {
+        if(root == NULL){
+            return true;
+        }
+
+        int leftH = height(root->left);
+        int rightH = height(root->right);
+
+        if(abs(leftH - rightH) > 1){
+            return false;
+        }
+
+        return isBalanced(root->left) && isBalanced(root->right);
+    }
+};
