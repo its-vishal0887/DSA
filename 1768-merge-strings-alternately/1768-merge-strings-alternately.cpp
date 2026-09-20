9e6110b61b90b41e454f5660012efc10aef65279
Vishal Sarvade <196059382+its-vishal0887@users.noreply.github.com>
2026-09-20 07:58:35 +0530

    Time: 0 ms (100%), Space: 8.8 MB (6.88%) - LeetHub

---
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    string mergeAlternately(string wrd1, string wrd2) {
+        string str = wrd1 + wrd2;
+        string ans = "";
+        int m = 0, n = 0, i = 0;
+        while (m < wrd1.size() && n < wrd2.size()) {
+            ans += wrd1[m++];
+            ans += wrd2[n++];
+        }
+        while (m < wrd1.size()) {
+            ans += wrd1[m];
+            m++;
+        }
+        while (n < wrd2.size()) {
+            ans += wrd2[n];
+            n++;
+        }
+        return ans;
+    }
+};
